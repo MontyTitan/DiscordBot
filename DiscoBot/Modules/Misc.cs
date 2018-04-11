f1ad9edd886158fedf0bf4f7ecdc2d8f30ef68e6
bryant <bryant.montgomery@intelligrated.com>
2018-04-11 15:30:04 -0400

    Clean up

--- DiscoBot/Modules/Misc.cs
+++ DiscoBot/Modules/Misc.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
-using DiscoBot.Core.UserAccounts;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
